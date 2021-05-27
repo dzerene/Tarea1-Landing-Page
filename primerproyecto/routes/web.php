cb3075b8ff85e57f834e0a5566a7fb8cdac4b872
panchoValdes <f.valdsmartnez>
2021-05-27 16:47:42 -0400

    avance vistade la tabla de los documentos

--- primerproyecto/routes/web.php
+++ primerproyecto/routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,10 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+
 Auth::routes();
 
 Route::middleware(['auth'])->group(function(){
     Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
     Route::get('/post/create', [App\Http\Controllers\PostController::class, 'create'])->name('post.create');
 });
+
+Route::get('vista1', function () {
+    //$users = App\User::all();
+    return view('vista1'/*, compact('users') */);
+});
+
+
 
