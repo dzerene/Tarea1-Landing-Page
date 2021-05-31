7ff4f60c80035b18a32317d67a7dc907aa1efa12
dzerene <62655513+dzerene@users.noreply.github.com>
2021-05-31 01:26:09 -0400

    Arreglada subida a base de datos

--- primerproyecto/resources/views/uploadfile.blade.php
+++ primerproyecto/resources/views/uploadfile.blade.php
@@ -13,7 +13,7 @@
                 <div class="card">
                   
                     <div class="card-body">
-                    <form method="POST" action="{{ route('file.store') }}" aria-label="{{ __('Upload') }}">@csrf
+                    <form method="POST" type="submit" action="{{ route('file.store') }}" aria-label="{{ __('Upload') }}">@csrf
                   
                           </div>
                             <div class="form-group row">
@@ -22,36 +22,15 @@
                                 <div id="file" class="dropzone"></div>
                               </div>    
                             </div>
-                            <div class="form-group row">
-                                <label for="title" class="col-sm-4 col-form-label text-md-right">{{ __('Nombre') }}</label>
-                                <div class="col-md-6">
-                                    <input id="title" type="text" class="form-control{{ $errors->has('title') ? ' is-invalid' : '' }}" name="title" value="{{ old('title') }}" required autofocus />
-                                    @if ($errors->has('title'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('title') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
-                            <div class="form-group row">
-                                <label for="overview" class="col-sm-4 col-form-label text-md-right">{{ __('Descripción') }}</label>
-                                <div class="col-md-6">
-                                    <textarea id="overview" cols="10" rows="10" class="form-control{{ $errors->has('overview') ? ' is-invalid' : '' }}" name="overview" value="{{ old('overview') }}" required autofocus></textarea>
-                                    @if ($errors->has('overview'))
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $errors->first('overview') }}</strong>
-                                        </span>
-                                    @endif
-                                </div>
-                            </div>
+                        
   
                                 </div>
                             </div>
                             <div class="form-group row mb-0">
                                 <div class="col-md-8 offset-md-4">
-                                    <button type="submit" href="/notification" class="btn btn-primary">
+                                    <a href="/notification" class="btn btn-primary">
                                         {{ __('Subir') }}
-                                    </button>
+                                    </a>
                                 
                                 <div class="panel-body" id="uploaded_image">
                                 </div>
