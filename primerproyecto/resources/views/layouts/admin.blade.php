01f2a9734eeb28465d671ccf29decea557b6590a
dzerene <dzerene@uandresbello.edu>
2021-05-31 20:22:55 -0400

    Actualización funcionalidad

--- primerproyecto/resources/views/layouts/admin.blade.php
+++ primerproyecto/resources/views/layouts/admin.blade.php
@@ -97,7 +97,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- Main Sidebar Container -->
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
       <!-- Brand Logo -->
-      <a href="{{ url('/welcome')}}" class="brand-link">
+      <a href="{{ url('/')}}" class="brand-link">
         <img src="../img/logo.png" alt="SoftPatagonia Logo" class="brand-image img-circle elevation-3"
           style="opacity: .8">
         <span class="brand-text font-weight-light">NotiLog</span>
@@ -200,8 +200,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
             </div><!-- /.col -->
             <div class="col-sm-6">
               <ol class="breadcrumb float-sm-right">
-                <li  class="breadcrumb-item"><a href="#">Home</a></li>                
-                <li class="breadcrumb-item active">Starter Page</li>
+                <li  class="breadcrumb-item"><a href="/home">Home</a></li>                
               </ol>
             </div><!-- /.col -->
           </div><!-- /.row -->
